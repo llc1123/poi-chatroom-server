34b1b27b7266348412e086c470e900be647d21fb
llc1123 <i@llc.moe>
2018-03-12 20:27:42 -0500

    change to ES6 syntax

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var socketio = require('socket.io');
-var server = require("http").createServer();
+const socketio = require('socket.io');
+const server = require("http").createServer();
 
-var port = 3000;
+const port = 3000;
 
-server.listen(port, function () {
+server.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
 
-var io = socketio.listen(server);
+const io = socketio.listen(server);
 
 // Chatroom
 
-var numUsers = 0;
+let numUsers = 0;
 
-io.sockets.on("connection", function(socket){
-	var addedUser = false;
+io.sockets.on("connection", (socket) => {
+	let addedUser = false;
 
-	socket.on('new message', function (data) {
+	socket.on('new message', (data) => {
     socket.broadcast.emit('new message', {
       username: socket.username,
       message: data,
@@ -28,15 +28,16 @@ io.sockets.on("connection", function(socket){
       trusted: "self",
       timestamp: Date.now()
     })
-    console.log(Date.now().toISOString() + '--' + socket.username + ': ' + data)
+    let d = new Date().toISOString()
+    console.log(d + '--' + socket.username + ': ' + data)
   });
 
-  socket.on('add user', function (data) {
+  socket.on('add user', (data) => {
     if (addedUser) return;
 
     socket.username = data.name;
     socket.trustedUser = data.real;
-    ++numUsers;
+    numUsers = numUsers + 1;
     addedUser = true;
     
     socket.emit('login', {
@@ -49,12 +50,13 @@ io.sockets.on("connection", function(socket){
       numUsers: numUsers,
       timestamp: Date.now()
     });
-    console.log(Date.now().toISOString() + '--' + socket.username + ' joined the chat, Current User: ' + numUsers)
+    let d = new Date().toISOString()
+    console.log(d + '--' + socket.username + ' joined the chat, Current User: ' + numUsers)
   });
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', () => {
     if (addedUser) {
-      --numUsers;
+      numUsers = numUsers - 1;
 
       socket.broadcast.emit('user left', {
         username: socket.username,
@@ -62,7 +64,8 @@ io.sockets.on("connection", function(socket){
         numUsers: numUsers,
         timestamp: Date.now()
       });
-      console.log(Date.now().toISOString() + '--' + socket.username + ' left the chat, Current User: '+ numUsers)
+      let d = new Date().toISOString()
+      console.log(d + '--' + socket.username + ' left the chat, Current User: '+ numUsers)
     }
   });
 });
